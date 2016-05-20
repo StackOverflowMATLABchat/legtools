f2b51b2c150b83bca3349e629766446e883c913c
sco1 <sco1.git@gmail.com>
2016-05-20 16:14:17 -0400

    Add legend entry removal method
    Resolves #1

--- legtools.m
+++ legtools.m
@@ -1,23 +1,24 @@
 classdef legtools
-% LEGTOOLS is a MATLAB class definition providing the user with a set of
-% methods to modify existing Legend objects.
-%
-% This is an HG2 specific implementation and requires MATLAB R2014b or
-% newer.
-%
-% legtools methods:
-%      append  - Adds string(s) to the end of the legend.
-%      permute - Rearrange the legend entries.
-%
-% See also legend
-
+    % LEGTOOLS is a MATLAB class definition providing the user with a set of
+    % methods to modify existing Legend objects.
+    %
+    % This is an HG2 specific implementation and requires MATLAB R2014b or
+    % newer.
+    %
+    % legtools methods:
+    %      append  - Add one or more entries to the end of the legend
+    %      permute - Rearrange the legend entries
+    %      remove  - Remove one or more legend entries
+    %
+    % See also legend
+    
     methods
         function obj = legtools
             % Dummy constructor so we don't return an empty class instance
             clear obj
         end
     end
-
+    
     methods (Static)
         function append(lh, newStrings)
             % APPEND appends strings, newStrings, to the specified Legend
@@ -32,14 +33,14 @@ classdef legtools
             % the associated axes object (e.g. if you have 2 lineseries and
             % 2 legend entries already no changes will be made).
             legtools.verchk()
-
+            
             % Make sure lh exists and is a legend object
             if ~exist('lh', 'var') || ~isa(lh, 'matlab.graphics.illustration.Legend')
                 error('legtools:append:InvalidLegendHandle', ...
                       'Invalid legend handle provided' ...
                       );
             end
-
+            
             % Pick first legend handle if more than one is passed
             if numel(lh) > 1
                 warning('legtools:append:TooManyLegends', ...
@@ -48,46 +49,55 @@ classdef legtools
                         );
                 lh = lh(1);
             end
-
+            
             % Make sure newString exists & isn't empty
             if ~exist('newStrings', 'var') || isempty(newStrings)
                 error('legtools:append:EmptyStringInput', ...
                       'No strings provided' ...
                       );
             end
-
+            
             % Validate the input strings
-            newStrings = legtools.fixstrings(newStrings);
-
+            if ischar(newStrings)
+                % Input string is a character array, assume it's a single
+                % string and dump into a cell
+                newStrings = {newStrings};
+            end
+            
+            % Check shape of newStrings and make sure it's 1D
+            if size(newStrings, 1) > 1
+                newStrings = reshape(newStrings', 1, []);
+            end
+            
             % To make sure we target the right axes, pull the legend's
             % PlotChildren and get their parent axes object
             parentaxes = lh.PlotChildren(1).Parent;
-
+            
             % Get line object handles
             plothandles = flipud(parentaxes.Children);  % Flip so order matches
-
+            
             % Update legend with line object handles & new string array
             newlegendstr = [lh.String newStrings];  % Need to generate this before adding new plot objects
             lh.PlotChildren = plothandles;
             lh.String = newlegendstr;
         end
-
-
+        
+        
         function permute(lh, order)
-            % PERMUTE rearranges the entries of the specified Legend 
-            % object, lh, so they are then the order specified by the 
-            % vector order. order must be the same length as the number of 
-            % legend entries in lh. All elements of order must be unique, 
+            % PERMUTE rearranges the entries of the specified Legend
+            % object, lh, so they are then the order specified by the
+            % vector order. order must be the same length as the number of
+            % legend entries in lh. All elements of order must be unique,
             % real, positive, integer values.
             legtools.verchk()
-
+            
             % Make sure lh exists and is a legend object
             if ~exist('lh', 'var') || ~isa(lh, 'matlab.graphics.illustration.Legend')
                 error('legtools:permute:InvalidLegendHandle', ...
                       'Invalid legend handle provided' ...
                       );
             end
-
+            
             % Pick first legend handle if more than one is passed
             if numel(lh) > 1
                 warning('legtools:permute:TooManyLegends', ...
@@ -96,27 +106,69 @@ classdef legtools
                         );
                 lh = lh(1);
             end
-
+            
             % Catch length & uniqueness issues with order, let MATLAB deal
             % with the rest.
             if numel(order) ~= numel(lh.String)
                 error('legtools:permute:TooManyIndices', ...
                       'Number of values in order must match the number of legend strings' ...
-                  );
+                      );
             end
             if numel(unique(order)) < numel(lh.String)
                 error('legtools:permute:NotEnoughUniqueIndices', ...
                       'order must contain enough unique indices to index all legend strings' ...
                       );
             end
-
+            
             % Permute the legend data source(s) and string(s)
             % MATLAB has a listener on the PlotChildren so when their order
             % is modified the string order is changed with it
             lh.PlotChildren = lh.PlotChildren(order);
         end
+        
+        
+        function remove(lh, remidx)
+            % REMOVE removes the legend indices at the locations specified
+            % by remidx. All elements of order must be real, positive,
+            % integer values.
+            % If remidx specifies all the legend entries the Legend
+            % object is deleted
+            legtools.verchk()
+            
+            % Make sure lh exists and is a legend object
+            if ~exist('lh', 'var') || ~isa(lh, 'matlab.graphics.illustration.Legend')
+                error('legtools:remove:InvalidLegendHandle', ...
+                      'Invalid legend handle provided' ...
+                      );
+            end
+            
+            % Pick first legend handle if more than one is passed
+            if numel(lh) > 1
+                warning('legtools:remove:TooManyLegends', ...
+                        '%u Legend objects specified, modifying the first one only', ...
+                        numel(lh) ...
+                        );
+                lh = lh(1);
+            end
+            
+            % Catch length issues, let MATLAB deal with the rest
+            if numel(unique(remidx)) > numel(lh.String)
+                error('legtools:remove:TooManyIndices', ...
+                      'Number of unique values in remidx must match the number of legend entries' ...
+                      );
+            end
+            
+            if numel(unique(remidx)) == numel(lh.String)
+                delete(lh);
+                warning('legtools:remove:LegendDeleted', ...
+                        'All legend entries specified for removal, deleting Legend Object' ...
+                        );
+            else
+                lh.PlotChildren(remidx) = [];
+            end
+        end
     end
-
+    
     methods (Static, Access = private)
         function verchk()
             % Throw error if we're not using R2014b or newer
@@ -124,21 +176,6 @@ classdef legtools
                 error('legtools:UnsupportedMATLABver', ...
                       'MATLAB releases prior to R2014b are not supported' ...
                       );
-            end
-        end
-
-
-        function [newstr] = fixstrings(oldstr)
-            newstr = oldstr;  % Lazy output initialization
-            if ischar(newstr)
-                % Input string is a character array, assume it's a single 
-                % string and dump into a cell
-                newstr = {newstr};
-            end
-
-            % Check shape of newStrings and make sure it's 1D
-            if size(newstr, 1) > 1
-                newstr = reshape(newstr', 1, []);
             end
         end
     end
