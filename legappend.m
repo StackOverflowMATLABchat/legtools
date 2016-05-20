6b150a009c827d998e998304cf448cc193f60038
sco1 <sco1.git@gmail.com>
2016-05-20 12:43:36 -0400

    Code Refactor

--- legappend.m
+++ legappend.m
@@ -20,46 +20,59 @@ if verLessThan('matlab','8.4')
           );
 end
 
+% Validate the input strings
+newStrings = checkstrings(newStrings);
+
+% Find the legend object to operate on
+lh = findlegend();
+
+% Add strings to legend
+newstr = [lh.String newStrings];
+appendtolegend(lh, newstr)
+end
+
+function appendtolegend(lh, newstr)
+% Get line object handles
+% To make sure we target the right axes, pull the current legend
+% PlotChildren and get the parent axes object
+parentaxes = lh.PlotChildren(1).Parent;
+plothandles = flipud(parentaxes.Children); % Flip so order matches
+
+% Update legend with line object handles & new string array
+lh.PlotChildren = plothandles;
+lh.String = newstr;
+end
+
+function newStrings = checkstrings(newStrings)
 % Make sure input isn't empty
 if ~exist('newStrings', 'var') || isempty(newStrings)
     error('legappend:EmptyInput', ...
           'No strings provided' ...
-          );
+         );
 end
 
-% Find our legend object
+if ischar(newStrings)
+    % Input string is a character array, assume it's a single string and
+    % dump into a cell
+    newStrings = {newStrings};
+end
+
+% Check shape of newStrings and make sure it's 1D
+if size(newStrings, 1) > 1
+    newStrings = reshape(newStrings', 1, []);
+end
+
+end
+
+function [lh] = findlegend()
 legendhandles = findobj(gcf, 'Type', 'legend');
 
 if ~isempty(legendhandles)
     % Operate only on the first legend handle returned
     lh = legendhandles(1);
-    
-    % Get existing array of legend strings and append our new string(s) to it
-    if ischar(newStrings)
-        % Input string is a character array, assume it's a single string and
-        % dump into a cell
-        newStrings = {newStrings};
-    end
-    
-    % Check shape of newStrings and make sure it's 1D
-    if size(newStrings, 1) > 1
-        newStrings = reshape(newStrings', 1, []);
-    end
-    newstr = [lh.String newStrings];
-
-    % Get line object handles
-    % To make sure we target the right axes, pull the current legend
-    % PlotChildren and get the parent axes object
-    parentaxes = lh.PlotChildren(1).Parent;
-    plothandles = flipud(parentaxes.Children); % Flip so order matches
-
-    % Update legend with line object handles & new string array
-    lh.PlotChildren = plothandles;
-    lh.String = newstr;
 else
-    % No legends in current figure
-    warning('legappend:NoLegend', ...
-            'No legend objects present in current figure' ...
-            );
+    error('legappend:NoLegend', ...
+          'No legend objects present in current figure' ...
+          );
 end
 end
