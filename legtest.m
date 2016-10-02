e3a54def607fcdf3c1bb603afd7b7733a3725767
Erik Huizinga <huizinga.erik@gmail.com>
2016-10-02 12:09:07 +0200

    Add bug description

--- legtest.m
+++ legtest.m
@@ -10,3 +10,6 @@ legtools.adddummy(legend,'dummy','ok')
 legtools.permute(legend,[3 2 1])
 legtools.remove(legend,2)
 legtools.append(legend,'sin')
+% Bug:
+% Append assumes the parent axes' children to be in the same order as the
+% legend entries, which may not be case if legtools.permute has been called
